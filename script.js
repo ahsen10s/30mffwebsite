29a23f5ed4d809c489a08ac1c7bc0a627329a58e
ahsen10s <as13805@nyu.edu>
2022-09-21 19:50:49 +0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// let ahsenImage = document.getElementById("ahsen-container");
+// let ahsenDescription = document.getElementById("ahsen-description-container");
+
+// let insiyaImage = document.getElementById("insiya-container");
+// let insiyaDescription = document.getElementById("insiya-description-container");
+
+// let zaidImage = document.getElementById("zaid-container");
+// let zaidDescription = document.getElementById("zaid-description-container");
+
+
+// ahsenImage.addEventListener('mouseenter', () => {
+//     ahsenDescription.style.opacity="orange"
+// })
+console.log("Hello fellow coder")
+
+
+let home = document.getElementById("home");
+let movie = document.getElementById("movie");
+let feedback = document.getElementById("feedback");
+let crew = document.getElementById("crew");
+
+
+home.addEventListener('mouseenter', () => {
+    home.style.color="orange"
+})
+home.addEventListener('mouseleave', () => {
+    home.style.color="black"
+})
+
+movie.addEventListener('mouseenter', () => {
+    movie.style.color="orange"
+})
+movie.addEventListener('mouseleave', () => {
+    movie.style.color="black"
+})
+
+feedback.addEventListener('mouseenter', () => {
+    feedback.style.color="orange"
+})
+feedback.addEventListener('mouseleave', () => {
+    feedback.style.color="black"
+})
+
+crew.addEventListener('mouseenter', () => {
+    crew.style.color="orange"
+})
+crew.addEventListener('mouseleave', () => {
+    crew.style.color="black"
+})
+
